ed757611e2e2bcb5f417d93d69a5bfacc6b1ed5d
Dominique Luna <dluna132@gmail.com>
2022-03-18 14:17:19 -0400

    update example

--- example/example.jl
+++ example/example.jl
@@ -1,20 +1,8 @@
-using JSON3
+module example
 
-module API
 using StructTypes
+
 include("graphqlgen_types.jl")
 include("graphqlgen_functions.jl")
-end;
 
-NODE_QUERY = """
-id
-"""
-f = API.node(NODE_QUERY)
-
-# Returns a GraphQL query and the variables required to execute it.
-x = f("10")
-
-# You can serialize the variables to JSON (as an example) and send use it
-# in an HTTP request.
-j = JSON3.write(x.variables)
-# output: "{\"id\":\"10\"}"
+end # module example
---
+++ example/test.jl
@@ -0,0 +1,28 @@
+using JSON3
+using Test
+
+# module name is example
+include("example.jl")
+
+NODE_QUERY = """
+id
+"""
+f = example.node(NODE_QUERY)
+
+# Returns a GraphQL query and the variables required to execute it.
+x = f("10")
+
+# You can serialize the variables to JSON (as an example) and send use it
+# in an HTTP request.
+j = JSON3.write(x.variables)
+# output: "{\"id\":\"10\"}"
+
+p = example.Person(; id = "22")
+edge = example.FilmCharactersEdge(p, "film")
+@test edge.node == p
+
+edge.node = nothing
+@test edge.node === nothing
+
+edge.node = missing
+@test edge.node === missing
